c9b6884002316d492fc98810229116f2559400cc
Julius <julius.schwartzenberg@gmail.com>
2013-09-26 20:46:54 +0200

    Make it compile

--- kicker/applets/menu/menuapplet.cpp
+++ kicker/applets/menu/menuapplet.cpp
@@ -38,6 +38,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <kglobalsettings.h>
 #include <klocale.h>
 #include <kwindowsystem.h>
+#include <KConfigGroup>
 
 #include <netwm.h>
 
