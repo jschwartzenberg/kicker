4dd4b3a67412cf2173301adaae58b0ccfaf1bbd6
Julius <julius.schwartzenberg@gmail.com>
2013-09-27 23:04:22 +0200

    Make it compile

--- kicker/libkicker/extensioninfo.cpp
+++ kicker/libkicker/extensioninfo.cpp
@@ -24,6 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <QFileInfo>
 #include <kdesktopfile.h>
 #include <krandom.h>
+#include <KConfigGroup>
 
 #include "extensioninfo.h"
 
