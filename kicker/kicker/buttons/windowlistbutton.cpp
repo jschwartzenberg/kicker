cbc62cee49de61119b995df7e1480f28a2981948
Julius <julius.schwartzenberg@gmail.com>
2013-09-27 23:00:53 +0200

    Make it compile

--- kicker/kicker/buttons/windowlistbutton.cpp
+++ kicker/kicker/buttons/windowlistbutton.cpp
@@ -23,7 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 
-#include <kwindowlistmenu.h>
+#include <kworkspace/kwindowlistmenu.h>
 #include <klocale.h>
 
 #include "windowlistbutton.h"
